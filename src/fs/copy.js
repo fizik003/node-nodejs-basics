333c820b71ea56af3c5bf0a40730770cbc134512
Yury <kassik0boks@gmail.com>
2024-10-07 00:48:15 +0300

    feat: added copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import * as fs from "fs/promises";
+import { getPathByDirname } from "../utils/fs.js";
+import { FSFiledError } from "../utils/error.js";
+
 const copy = async () => {
-    // Write your code here 
+  const META_URL = import.meta.url;
+  const SOURCE_FOLDER_NAME = "files";
+  const DESTINATION_FOLDER_NAME = "files_copy";
+  const srcFolderPath = getPathByDirname(META_URL, SOURCE_FOLDER_NAME);
+  const destFolderPath = getPathByDirname(META_URL, DESTINATION_FOLDER_NAME);
+  try {
+    await fs.access(srcFolderPath);
+  } catch (error) {
+    throw new FSFiledError();
+  }
+
+  try {
+    await fs.access(destFolderPath);
+    throw new FSFiledError();
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      throw error;
+    }
+  }
+
+  try {
+    fs.cp(srcFolderPath, destFolderPath, { recursive: true });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await copy();
