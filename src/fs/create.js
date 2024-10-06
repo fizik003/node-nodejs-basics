bbf6cc30cf0f06b38d4d96d5b626c11c6cb7321c
Yury <kassik0boks@gmail.com>
2024-10-06 20:26:29 +0300

    feat: added create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const EXIST_ERROR_CODE = "EEXIST";
+  const content = "I am fresh and young";
+  const EXIST_ERROR_MESSAGE = "FS operation failed";
+  const newFilePath = join(__dirname, "files", "fresh.txt");
+
+  try {
+    await writeFile(newFilePath, content, { flag: "wx" });
+  } catch (error) {
+    if (error.code === EXIST_ERROR_CODE) {
+      throw new Error(EXIST_ERROR_MESSAGE);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
