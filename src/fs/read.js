1dddfb66b26c02d8aea586c5b6825818639e763b
Yury <kassik0boks@gmail.com>
2024-10-07 18:21:22 +0300

    feat: added read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import * as fs from "fs/promises";
+import { getPathByDirname } from "../utils/fs.js";
+import { throwNoSuchFileError } from "../utils/error.js";
+
 const read = async () => {
-    // Write your code here 
+  const META_URL = import.meta.url;
+  const src = getPathByDirname(META_URL, "files", "fileToRead.txt");
+  try {
+    const fileContent = await fs.readFile(src, "utf-8");
+    console.log(fileContent);
+  } catch (error) {
+    throwNoSuchFileError(error);
+    throw error;
+  }
 };
 
 await read();
