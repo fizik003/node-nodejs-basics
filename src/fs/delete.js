87fdd9371722f2cff1365017465dc9a09c6d1ae5
Yury <kassik0boks@gmail.com>
2024-10-07 18:01:45 +0300

    feat: added delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import * as fs from "fs/promises";
+import { getPathByDirname } from "../utils/fs.js";
+import { FSFiledError, throwNoSuchFileError } from "../utils/error.js";
+
 const remove = async () => {
-    // Write your code here 
+  const META_URL = import.meta.url;
+  const srcPath = getPathByDirname(META_URL, "files", "fileToRemove.txt");
+  try {
+    await fs.unlink(srcPath);
+  } catch (error) {
+    throwNoSuchFileError(error);
+    throw error;
+  }
 };
 
 await remove();
