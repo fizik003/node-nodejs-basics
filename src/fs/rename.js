cc7b5284a560b273cde5f15a10a47a0864aa597e
Yury <kassik0boks@gmail.com>
2024-10-07 17:50:46 +0300

    feat: added rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import * as fs from "fs/promises";
+import { getPathByDirname } from "../utils/fs.js";
+import { FSFiledError } from "../utils/error.js";
+
 const rename = async () => {
-    // Write your code here 
+  const META_URL = import.meta.url;
+  const srcPath = getPathByDirname(META_URL, "files", "wrongFilename.txt");
+  const destPath = getPathByDirname(META_URL, "files", "properFilename.md");
+  try {
+    await fs.rename(srcPath, destPath);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new FSFiledError();
+    }
+    throw error;
+  }
 };
 
 await rename();
