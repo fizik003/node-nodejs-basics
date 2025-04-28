0a0574ec352890734e906e27ed69f103b3435f7e
Yury <kassik0boks@gmail.com>
2025-04-28 23:33:08 +0300

    feat: aaded close worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -5,6 +5,7 @@ const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2
 const sendResult = (n) => {
     const result = nthFibonacci(n);
     parentPort.postMessage(result);
+    parentPort.close()
 };
 
 parentPort.on('message', (n) => {
